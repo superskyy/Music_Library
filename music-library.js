1829f8afafe8d93f6e4a7bbc38c5c1867852ea17
Peter <psynevskyy.com>
2019-06-07 19:52:02 +0000

    added functions

---
+++ music-library.js
@@ -0,0 +1,35 @@
+function MusicLibrary (name, creator) {
+	this.name = name;
+	this.creator = creator;
+	this.playlist = [];
+}
+
+function Playlist (name) {
+	this.name = name;
+	this.tracks = [];
+}
+
+function Tracks (title, rating, length) {
+	this.title = title;
+	this.rating = rating;
+	this.length = length;
+}
+
+const artist = new MusicLibrary('Peter', "Syn")
+
+
+
+
+
+
+
+
+
+
+// function Playlist () {
+
+// }
+
+// function Track () {
+
+// }
